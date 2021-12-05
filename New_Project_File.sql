eb0c941c894f9a27025393e48c08eae2b2bc15ff
Stephen <rayx70@live.ca>
2021-12-05 13:30:30 -0500

    Update New_Project_File.sql

--- New_Project_File.sql
+++ New_Project_File.sql
@@ -94,46 +94,45 @@ VALUES (943218900, 'Levi Ackerman', '9 Sentinel Ave', 'CoolEmail5@hotmail.com', 
 INSERT INTO Student(Student_number, Full_name, Address, Email, Phone_number, Admission_year, GPA, Num_credits_taken, International_Student)
 VALUES (123412341, 'Stephen Morris', '59 Stanley St', 'rayx70@live.ca', '226-910-1111', 2019, 7.2, 12, FALSE);
 
-4
 INSERT INTO Student(Student_number, Full_name, Address, Email, Phone_number, Admission_year, GPA, Num_credits_taken, International_Student)
 VALUES (654333321, 'Kevin Morris', '100 Park St', 'KevilMorris21@live.ca', '519-110-1010', 2019, 9.2, 12, FALSE);
-5
+
 INSERT INTO Student(Student_number, Full_name, Address, Email, Phone_number, Admission_year, GPA, Num_credits_taken, International_Student)
 VALUES (654511100, 'Zaid Matthews', '2 Cool St', 'wahzoo@gmail.com', '231-110-9942', 2021, 11.2, 11.7, TRUE);
-6
+
 INSERT INTO Student(Student_number, Full_name, Address, Email, Phone_number, Admission_year, GPA, Num_credits_taken, International_Student)
 VALUES (654300004, 'Billy Bob', '29 Park St', 'billBob21213@gmail.com', '519-987-2219', 2020, 8.5, 12, FALSE);
-7
+
 INSERT INTO Student(Student_number, Full_name, Address, Email, Phone_number, Admission_year, GPA, Num_credits_taken, International_Student)
 VALUES (999988881, 'Nick Lindstrom', '34 Cresent Road', 'pro54@live.ca', '443-212-2021', 2017, 6.9, 21, TRUE);
-8
+
 INSERT INTO Student(Student_number, Full_name, Address, Email, Phone_number, Admission_year, GPA, Num_credits_taken, International_Student)
 VALUES (120946754, 'Allen Walker', '9 Moon St', 'AllenWalker@gmail.com', '519-100-5477', 2020, 8, 10, FALSE);
-9
+
 INSERT INTO Student(Student_number, Full_name, Address, Email, Phone_number, Admission_year, GPA, Num_credits_taken, International_Student)
 VALUES (432334444, 'Caroline Parker', '45 North St', 'CoolEmail@live.ca', '519-432-7410', 2018, 9, 19, FALSE);
-10
+
 INSERT INTO Student(Student_number, Full_name, Address, Email, Phone_number, Admission_year, GPA, Num_credits_taken, International_Student)
 VALUES (328900012, 'Farnese Bell', '98 South St', 'myEmail@gmail.com', '519-332-1029', 2020, 8, 12, FALSE);
-11
+
 INSERT INTO Student(Student_number, Full_name, Address, Email, Phone_number, Admission_year, GPA, Num_credits_taken, International_Student)
 VALUES (555992111, 'Rick Hall', '3 New St', 'rickHall@gmail.com', '226-954-1109', 2020, 9, 14, FALSE);
-12
+
 INSERT INTO Student(Student_number, Full_name, Address, Email, Phone_number, Admission_year, GPA, Num_credits_taken, International_Student)
 VALUES (543216543, 'Nisio Isin', '33 Old St', 'NisioIsin@gmail.com', '233-001-4431', 2021, 9.1, 1, FALSE);
-13
+
 INSERT INTO Student(Student_number, Full_name, Address, Email, Phone_number, Admission_year, GPA, Num_credits_taken, International_Student)
 VALUES (543222222, 'Zoe Brown', '127 York St', 'ZoeBrown@gmail.com', '999-926-5551', 2020, 9.3, 8, TRUE);
-14
+
 INSERT INTO Student(Student_number, Full_name, Address, Email, Phone_number, Admission_year, GPA, Num_credits_taken, International_Student)
 VALUES (110011001, 'Nolan Smith', '92 East St', 'NolanSmith@gmail.com', '226-019-2412', 2021, 12, 4, FALSE);
-15
+
 INSERT INTO Student(Student_number, Full_name, Address, Email, Phone_number, Admission_year, GPA, Num_credits_taken, International_Student)
 VALUES (102938474, 'Alex Williams', '24 West St', 'AlexMiller@gmail.com', '999-239-0001', 2018, 6.5, 19, TRUE);
-16
+
 INSERT INTO Student(Student_number, Full_name, Address, Email, Phone_number, Admission_year, GPA, Num_credits_taken, International_Student)
 VALUES (193143333, 'Stephen Williams', '54 Lancaster St', 'SteveWilliams@gmail.com', '519-299-1120', 2019, 7.5, 15, FALSE);
-17
+
 INSERT INTO Student(Student_number, Full_name, Address, Email, Phone_number, Admission_year, GPA, Num_credits_taken, International_Student)
 VALUES (192938424, 'Emma Smith', '1 University Ave', 'Emma543@gmail.com', '519-948-5431', 2018, 11.4, 19, FALSE);
 
@@ -312,7 +311,33 @@ INSERT INTO Student_Major(Student_Major_id, Major_id, Major_GPA, student_number)
 VALUES (8,802, 9, 120946754);
 
 INSERT INTO Student_Major(Student_Major_id, Major_id, Major_GPA, student_number)
-VALUES (9,902 9, 432334444);
+VALUES (9,902, 9, 432334444);
+
+INSERT INTO Student_Major(Student_Major_id, Major_id, Major_GPA, student_number)
+VALUES (10,802 ,9, 328900012);
+
+INSERT INTO Student_Major(Student_Major_id, Major_id, Major_GPA, student_number)
+VALUES (11,802, 9, 555992111);
+
+INSERT INTO Student_Major(Student_Major_id, Major_id, Major_GPA, student_number)
+VALUES (12,901, 8, 543216543);
+
+INSERT INTO Student_Major(Student_Major_id, Major_id, Major_GPA, student_number)
+VALUES (13,101 ,11, 543222222);
+
+INSERT INTO Student_Major(Student_Major_id, Major_id, Major_GPA, student_number)
+VALUES (14,101, 9.5, 110011001);
+
+INSERT INTO Student_Major(Student_Major_id, Major_id, Major_GPA, student_number)
+VALUES (15,101, 10, 102938474);
+
+INSERT INTO Student_Major(Student_Major_id, Major_id, Major_GPA, student_number)
+VALUES (16,101, 11.3, 193143333);
+
+INSERT INTO Student_Major(Student_Major_id, Major_id, Major_GPA, student_number)
+VALUES (17,101, 11.5, 192938424);
+
+
 
 
 --Data for Minor:
@@ -327,19 +352,11 @@ INSERT INTO Minor(Minor_id, Minor)
 VALUES (1111, 'Criminology');
 
 INSERT INTO Minor(Minor_id, Minor)
-VALUES (2222, 'Music');
-
-INSERT INTO Minor(Minor_id, Minor)
-VALUES (3333, 'Music');
-
-INSERT INTO Minor(Minor_id, Minor)
 VALUES (1231, 'Creative Writing');
 
 INSERT INTO Minor(Minor_id, Minor)
 VALUES (8484, 'User experience');
 
-INSERT INTO Minor(Minor_id, Minor)
-VALUES (6532, 'User experience');
 
 
 -- DATA for Student_minor;
@@ -351,22 +368,22 @@ INSERT INTO Student_Minor(Student_Minor_id, Minor_id, Student_number)
 VALUES (2, 3321, 943218900);
 
 INSERT INTO Student_Minor(Student_Minor_id, Minor_id, Student_number)
-VALUES (3, 1111, 943218900);
+VALUES (3, 1111, 123412341);
 
 INSERT INTO Student_Minor(Student_Minor_id, Minor_id, Student_number)
-VALUES (4, 2222, 943218900);
+VALUES (4, 3214, 654333321);
 
 INSERT INTO Student_Minor(Student_Minor_id, Minor_id, Student_number)
-VALUES (5, 3333, 943218900);
+VALUES (5, 3214, 654511100);
 
 INSERT INTO Student_Minor(Student_Minor_id, Minor_id, Student_number)
-VALUES (6, 1231, 943218900);
+VALUES (6, 1231, 654300004);
 
 INSERT INTO Student_Minor(Student_Minor_id, Minor_id, Student_number)
-VALUES (7, 8484, 943218900);
+VALUES (7, 8484, 999988881);
 
 INSERT INTO Student_Minor(Student_Minor_id, Minor_id, Student_number)
-VALUES (8, 6532, 943218900);
+VALUES (8, 8484, 120946754);
 
 
 
@@ -430,14 +447,14 @@ WITH temp AS (SELECT SUM(OSAP) AS TOTAL
 SELECT Major.Major_id, Student.Full_name, Student.GPA
 FROM Student
 INNER JOIN Major
-ON Student.Student_number=Major.Student_number
+ON Student.Student_number=Student_Major.Student_number
 WHERE Major = 'Computer Science' AND GPA >= 9;
 
 
 -- Case 3:
 SELECT Student_number, Full_name, Email, Admission_year, GPA, International_Student, Major, Minor, Scholarship_amount, Scholarship_Type, OSAP
 FROM (SELECT Student_number, Major, Minor, Scholarship_Amount, Scholarship_Type, OSAP
-FROM Major natural join Minor natural join Financial
+FROM Major natural join Minor natural join Scholarship 
 WHERE minor.Student_number = '120946754') as x natural join Student natural join Courses
 WHERE student.Full_name = 'Allen Walker' AND student.Student_number = '120946754';
 
